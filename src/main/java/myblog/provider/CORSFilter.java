e80e4fe62bc346650fefd13c5f8fa827589cde72
inxi <276692580@qq.com>
2016-10-28 15:13:38 +0800

    Format code

--- src/main/java/myblog/provider/CORSFilter.java
+++ src/main/java/myblog/provider/CORSFilter.java
@@ -8,14 +8,12 @@ import java.io.IOException;
 
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
+
     public void filter(ContainerRequestContext containerRequestContext,
-                       ContainerResponseContext containerResponseContext)
-            throws IOException {
+                       ContainerResponseContext containerResponseContext) throws IOException {
         containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers",
-                "origin, content-type, accept, authorization");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         containerResponseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        containerResponseContext.getHeaders().add("Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
