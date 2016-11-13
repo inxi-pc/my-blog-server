5dc867a6f5404f8ffc43a5fadabb453a9a64e615
inxi-pc <276692580@qq.com>
2016-11-13 17:00:18 +0800

    修改跨域origin值，对应xhr.withCredential = true可以生效

--- src/main/java/myblog/provider/CORSFilter.java
+++ src/main/java/myblog/provider/CORSFilter.java
@@ -11,7 +11,8 @@ public class CORSFilter implements ContainerResponseFilter {
 
     public void filter(ContainerRequestContext containerRequestContext,
                        ContainerResponseContext containerResponseContext) throws IOException {
-        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        String origin = (String) containerRequestContext.getHeaders().getFirst("origin");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", origin);
         containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         containerResponseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
