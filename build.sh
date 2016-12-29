2651d551075514fd1437ebb51fae9dfaa732845b
inxi <276692580@qq.com>
2016-12-29 18:25:34 +0800

    Optimize deploy shell script

--- build.sh
+++ build.sh
@@ -1,32 +1,54 @@
 #!/bin/bash
 
-cur_dir=`pwd`
-conf_dir=$cur_dir/src/main/resources
 env='development'
+
+VALID_ENV=('production' 'development')
+
+function check_env {
+    exist=0
+    for i in "${VALID_ENV[@]}"
+    do
+        if [[ $i = $1 ]]; then
+            exist=1
+        fi
+    done
+    if [[ $exist = 0 ]]; then
+        error_exit "`date "+%Y-%m-%d% %H:%M:%S"` Error: Not valid region, Got $1"
+    fi
+}
+
+function error_exit {
+    echo $1 1>&2
+    exit 1
+}
+
+function echo_with_date {
+    echo "`date "+%Y-%m-%d% %H:%M:%S"` Info: $1"
+}
 
 i=0
 while [ $i -le $# ];do
     case ${!i} in
         -env)
-            i=$(expr $i + 1)
+            shift
             env=${!i}
+            check_env $env
             env=${env:-development}
         ;;
     esac
     i=$(expr $i + 1)
 done
 
+echo_with_date "Grunt.js task running..."
 if [ $env = 'development' ]; then
-    echo 'Begin set development config......'
     grunt build:dev
     mvn clean package
 else
-    echo 'Begin set production config......'
     grunt build:prod
     mvn clean package
 fi
 
-echo 'Build Successful'
+echo_with_date 'End task with success status'
 exit
 
 
